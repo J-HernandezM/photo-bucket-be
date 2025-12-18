432f886c57ca93bff8cc67c9ffa78d90068f562b
juan <juansw03@gmail.com>
2025-12-18 01:37:37 -0500

    refactor: s3 client expiration now defined within method. upload uses uploadobject instead of path for the file

--- app/clients/s3_client.py
+++ app/clients/s3_client.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 import asyncio
 from contextlib import asynccontextmanager
-from pathlib import Path
 
 from aioboto3 import Session
+from fastapi import UploadFile
 from types_aiobotocore_s3 import S3Client
 
 from app.config import settings
@@ -13,27 +13,26 @@ logger = settings.logger
 
 class AwsS3ClientInterface(ABC):
     @abstractmethod
-    async def upload_file(self, path_to_s3: str, file: Path) -> str:
-        """Uploads a file to the s3 bucket and returns it's URI"""
+    async def upload_file(self, path_to_s3: str, file: UploadFile) -> str:
+        """Uploads a file to the s3 bucket and returns it's S3 key"""
         pass
 
     @abstractmethod
-    async def bulk_upload_file(self, path_to_s3: str, files: list[Path]) -> list[str]:
-        """Uploads a group of files to the s3 bucket and return a list containing the UIR or an empty string for failed uploads"""
+    async def bulk_upload_file(
+        self, path_to_s3: str, files: list[UploadFile]
+    ) -> list[str]:
+        """Uploads a group of files to the s3 bucket and return a list containing the S3 keys or an empty string for failed uploads"""
         pass
 
     @abstractmethod
-    async def get_file_presigned_url(self, path_to_s3: str, filename: str) -> str:
-        """Fetch a file from the s3 bucket and return it's URI"""
+    async def get_file_presigned_url(self, key: str) -> str:
+        """Fetch a file from the s3 bucket and return a presigned URL"""
 
 
 class AwsS3Client(AwsS3ClientInterface):
-    def __init__(
-        self, session: Session, bucket: str, presigned_expiration_in_seconds: int = 3600
-    ):
+    def __init__(self, session: Session, bucket: str):
         self._session = session
         self._bucket = bucket
-        self._expiration = presigned_expiration_in_seconds
 
     @asynccontextmanager
     async def _get_client(self):
@@ -46,50 +45,55 @@ class AwsS3Client(AwsS3ClientInterface):
             yield client
 
     async def _upload_file(
-        self, path_to_s3: str, file: Path, s3_client: S3Client
+        self, path_to_s3: str, file: UploadFile, s3_client: S3Client
     ) -> str:
         prefix = path_to_s3.strip("/")
-        key = f"{prefix}/{file.name}" if prefix else file.name
+        key = f"{prefix}/{file.filename}" if prefix else file.filename
+        content_type = file.content_type
+        file_obj = file.file
+        extra = {"ContentType": content_type} if content_type else None
 
         try:
             logger.info(f"Uploading {key} to bucket: {self._bucket}")
 
-            with file.open("rb") as f:
-                await s3_client.upload_fileobj(Fileobj=f, Bucket=self._bucket, Key=key)
-                logger.info("File uploaded successfully")
+            await s3_client.upload_fileobj(
+                Fileobj=file_obj, Bucket=self._bucket, Key=key, ExtraArgs=extra
+            )
+            logger.info("File uploaded successfully")
         except Exception as e:
             logger.error(
                 f"Unable to upload {key} to bucket: {self._bucket} {e} ({type(e)})"
             )
             return ""
 
-        return f"s3://{self._bucket}/{key}"
+        return key
 
-    async def upload_file(self, path_to_s3: str, file: Path) -> str:
+    async def upload_file(self, path_to_s3: str, file: UploadFile) -> str:
         async with self._get_client() as s3_client:
-            urn = await self._upload_file(path_to_s3, file=file, s3_client=s3_client)
-        return urn
+            s3_path = await self._upload_file(
+                path_to_s3, file=file, s3_client=s3_client
+            )
+        return s3_path
 
-    async def bulk_upload_file(self, path_to_s3: str, files: list[Path]) -> list[str]:
+    async def bulk_upload_file(
+        self, path_to_s3: str, files: list[UploadFile]
+    ) -> list[str]:
         async with self._get_client() as s3_client:
             tasks = [
                 self._upload_file(path_to_s3=path_to_s3, file=f, s3_client=s3_client)
                 for f in files
             ]
-            uris = await asyncio.gather(*tasks)
+            s3_paths = await asyncio.gather(*tasks)
 
-        return uris
+        return s3_paths
 
-    async def get_file_presigned_url(self, path_to_s3: str, filename: str):
-        prefix = path_to_s3.strip("/")
-        key = f"{prefix}/{filename}" if prefix else filename
-
+    async def get_file_presigned_url(self, key: str, expiration: int = 3600):
         async with self._get_client() as s3_client:
             try:
                 url = await s3_client.generate_presigned_url(
                     "get_object",
                     Params={"Bucket": self._bucket, "Key": key},
-                    ExpiresIn=self._expiration,
+                    ExpiresIn=expiration,
                 )
 
                 return url
